53c1c8617482e08a622c706c5bf0c2ebb0e19219
Alex Shcherbakov <oleksa.shcherbakov@gmail.com>
2024-11-02 16:00:29 +0200

    changed settings for static files

--- newspaper_agency/settings/base.py
+++ newspaper_agency/settings/base.py
@@ -121,12 +121,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 ASSETS_ROOT = "/static/assets"
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
